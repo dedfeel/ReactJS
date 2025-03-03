35c5a2262e25607caf7c046ea5548e41af15aa2a
Aziz <baziz1275@gmail.com>
2025-03-03 23:23:50 +0500

    reactProject

---
+++ ProfileCard/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='container'>
+      <h1 className='title'>Менің визит картам</h1>
+      <div className='div'>
+        <h2 className='name'>Аты жөні: Aziz</h2>
+        <p className='name'>Мамандығы: FullStuck</p>
+        <p className='name'>Қала: Almaty, KZ</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
